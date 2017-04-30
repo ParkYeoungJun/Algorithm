4db9f990e92747e64468786fe4b4a7887c4716c1
YeoungJun Park <YeoungJun Park>
2017-04-30 19:11:24 +0900

    1038 progressing

---
+++ progressing_1038_decrease_num.cpp
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<iostream>
+#include<stdlib.h>
+#include<string>
+
+using namespace std;
+
+int main()
+{
+	int n, count = 0;
+
+	scanf("%d", &n);
+
+	if (n < 10 && n >= 0)
+		printf("%d", n);
+	else
+	{
+
+	}
+}
