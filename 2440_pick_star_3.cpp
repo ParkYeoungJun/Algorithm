883052ac9f45f1908a28623b53336a124e554068
YeoungJun Park <YeoungJun Park>
2017-07-12 11:15:56 +0900

    commit from labtop

---
+++ 2440_pick_star_3.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+#include<cstdio>
+
+using namespace std;
+
+int main()
+{
+    int n;
+
+    scanf("%d", &n);
+
+    for(int i = n ; i >= 1 ; --i)
+    {
+        for(int j = i ; j >= 1 ; --j)
+        {
+            printf("*");
+        }
+
+        printf("\n");
+    }
+}
