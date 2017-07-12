406c2ca75a02dcc9e0da6871d478f26be79a58a6
YeoungJun Park <YeoungJun Park>
2017-07-12 12:01:17 +0900

    commit from labtop

---
+++ 8383_sum_n.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+#include<cstdio>
+
+using namespace std;
+
+int main()
+{
+    int n;
+    long long int sum = 0;
+
+    scanf("%d", &n);
+
+    for(int i = 1 ; i <= n ; ++i)
+        sum += i;
+
+    printf("%lld", sum);
+}
