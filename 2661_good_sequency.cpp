6e69831db37772692e8ccd3cdfe9aace31de4fe8
ParkYeoungJun <dudwns4971@gmail.com>
2017-08-29 00:56:25 +0900

    2661 done

---
+++ 2661_good_sequency.cpp
@@ -0,0 +1,38 @@
+#include<cstdio>
+#include<algorithm>
+
+using namespace std;
+
+int n, a[85];
+
+void dfs(int cnt) 
+{
+	for (int i = 1; i <= cnt / 2; i++) 
+    {
+		if (equal(a + cnt - i, a + cnt, a + cnt - (i*2))) 
+            return;
+	}
+
+	if (cnt == n) 
+    {
+		for (int i = 0; i < n; i++) 
+            printf("%d", a[i]);
+		
+        exit(0);
+	}
+
+	for (int i = 1; i <= 3; i++) 
+    {
+		a[cnt] = i;
+		dfs(cnt + 1);
+	}
+}
+
+int main() 
+{
+	scanf("%d", &n);
+
+	dfs(0);
+
+	return 0;
+}
