e68f277eefc4e6a176ce549bef7f54e70c3b344e
ParkYeoungJun <dudwns4971@gmail.com>
2017-09-06 14:16:48 +0900

    1931_alloc.cpp done

--- 2217_rope.cpp
+++ 2217_rope.cpp
@@ -30,4 +30,36 @@ int main()
     }
 
     printf("%d", maxi);
+}#include<iostream>
+#include<cstdio>
+#include<algorithm>
+#include<vector>
+
+using namespace std;
+
+vector<int> arr;
+
+int main()
+{
+    int n;
+    int mini = 10000;
+    int maxi = 0;
+
+    scanf("%d", &n);
+
+    for(int i = 0 ; i < n ; ++i)
+    {
+        int tmp;
+        scanf("%d", &tmp);
+        arr.push_back(tmp);
+    }
+
+    sort(arr.begin(), arr.end());
+
+    for(int i = 0 ; i < n ; ++i)
+    {
+        maxi = max(arr[i]*(n-i), maxi);
+    }
+
+    printf("%d", maxi);
 }
